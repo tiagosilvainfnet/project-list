b821ff14ad9c30b5a7af89ebc1bd57814a49c8fb
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2024-08-19 21:59:34 -0300

    Tela de login

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,21 +1,89 @@
-import {Text, View} from "react-native";
+import {ScrollView, Text, View} from "react-native";
 import {Button} from "react-native-paper";
-import {router} from "expo-router";
-import React, {useEffect} from "react";
+import {Link, router} from "expo-router";
+import React, {useEffect, useState} from "react";
 import {useSession} from "@/app/ctx";
+import {Avatar, TextInput} from "@/components";
 
 // @ts-ignore
 const SignIn = () => {
     const { signIn } = useSession();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    return <View style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100%',
-            }}>
-                <Button onPress={signIn}>Login</Button>
-           </View>
+    return <ScrollView>
+                <View style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    height: '100%',
+                }}>
+                    <View style={{
+                        width: '100%',
+                        marginTop: 100,
+                        ...styles.container
+                    }}>
+                        <Avatar size={200} source={require('../assets/images/logo.jpg')}/>
+                    </View>
+                    <View style={{
+                        width: '100%',
+                        textAlign: 'center',
+                        ...styles.padding,
+                        ...styles.container
+                    }}>
+                        <Text style={{
+                            fontSize: 24
+                        }}>Seja bem-vindo!</Text>
+                    </View>
+                    <View style={{
+                        width: '100%',
+                        ...styles.padding
+                    }}>
+                        <TextInput
+                            keyboardType="email-address"
+                            label="E-mail"
+                            value={email}
+                            onChangeText={setEmail}
+                        />
+                    </View>
+                    <View style={{
+                        width: '100%',
+                        ...styles.padding
+                    }}>
+                        <TextInput
+                            label="Senha"
+                            secureTextEntry={true}
+                            value={password}
+                            onChangeText={setPassword}
+                        />
+                    </View>
+                    <View style={{
+                        width: '100%',
+                        textAlign: 'center',
+                        ...styles.container
+                    }}>
+                        <Link href="register">Cadastrar</Link>
+                    </View>
+                    <View style={{
+                        width: '100%',
+                        ...styles.padding
+                    }}>
+                        <Button mode="contained" onPress={signIn}>
+                            Entrar
+                        </Button>
+                    </View>
+                </View>
+            </ScrollView>
+}
+
+const styles = {
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    padding: {
+        padding: 16
+    }
 }
 
 export default SignIn;
